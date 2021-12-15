81d5de26c6d808a453c881820bf4a1918c382b0d
Dara Anya <daraanya@gmail.com>
2021-12-15 21:48:39 +0100

    commit on 'master'

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,5 @@
 #include <iostream> 
+#include <ctime> // accesses computer's time
 
 /* 
 In this game there are three numbers in a code
@@ -7,26 +8,28 @@ The code adds-up to give --
 The user must guess the three digit code
 */
 
-void PrintIntroduction(){
-    std::cout << "\n\n Upon boarding the abondoned alien vessel, you see a computer terminal" <<LevelDifficulty;
-    std::cout << "\n You need to enter the corret code to access the ship's log...\n";
+void PrintIntroduction(int Difficulty){
+    std::cout << "\n\nUpon boarding the abondoned alien vessel, you see a level " << Difficulty;
+    std::cout << " computer terminal. \n\nYou need to enter the corret code to access the ship's log...\n";
 }
-bool PlayGame(){
-    PrintIntroduction();
+bool PlayGame(int Difficulty){
+    PrintIntroduction(Difficulty);
 
-    const int CodeA = 4; 
-    const int CodeB = 16;
-    const int CodeC = 32;
+    const int CodeA = rand() % Difficulty + Difficulty; //Add '1' to the modular to remove '0' from the randomization range
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
+
+    std::cout << CodeA << CodeB << CodeC;
 
     const int CodeSum = CodeA + CodeB + CodeC; 
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     std::cout << "\nThere are three numbers in the code\n";
-    std::cout << "\n The codes add-up to: " << CodeSum;
-    std::cout << "\n The codes multiply to give: " << CodeProduct;
+    std::cout << "\nThe codes add-up to: " << CodeSum;
+    std::cout << "\nThe codes multiply to give: " << CodeProduct;
 
     int GuessA, GuessB, GuessC; 
-    std::cout << "\n\n Guess the three-number code. Insert a space between each number:\n";
+    std::cout << "\n\nGuess the three-number code. Insert a space between each number:\n";
     // Store player guess
     std::cin >> GuessA >> GuessB >> GuessC;
 
@@ -51,9 +54,13 @@ bool PlayGame(){
 }
 
 int main(){
+    srand(time(NULL)); //Creates new random sequences based on the time of day
     int LevelDifficulty = 1;
-    while (true){
-        bool bLevelComplete = PlayGame(); //All boolean variables must start with lowercase 'b' in Unreal Engine
+    const int MaxDifficulty = 3;
+
+    //Loop game until all levels completed
+    while (LevelDifficulty <= MaxDifficulty){ 
+        bool bLevelComplete = PlayGame(LevelDifficulty); //All boolean variables must start with lowercase 'b' in Unreal Engine
 
         // notice if input letter for GuessCode creates endless loop --> this causes an error
         std::cin.clear(); //Clears any errors
@@ -62,10 +69,16 @@ int main(){
         if (bLevelComplete){
             //increase level difficulty
             LevelDifficulty ++;
+            if(((MaxDifficulty + 1)- LevelDifficulty) == 1){
+                std::cout << "There is " << ((MaxDifficulty + 1)- LevelDifficulty) << " lock remaining.";
+
+            } else{
+                std::cout << "There are " << ((MaxDifficulty + 1)- LevelDifficulty) << " locks remaining.";
+            }
 
         }
-        
     }
 
+    std::cout << "You have access to the terminal";
     return 0; 
 }
