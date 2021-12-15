3ba5849ae61fb5afea886ea2def1528d7082ade9
Dara Anya <daraanya@gmail.com>
2021-12-15 13:11:25 +0100

    commit on master

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,3 +1,5 @@
+#include <iostream> 
+
 /* 
 In this game there are three numbers in a code
 The code multiplies to give --
@@ -5,14 +7,11 @@ The code adds-up to give --
 The user must guess the three digit code
 */
 
-#include <iostream> 
-
-
 void PrintIntroduction(){
-    std::cout << "\n Upon boarding the abondoned alien vessel, you see a computer terminal...\n";
-    std::cout << "You need to enter the corret code to access the ship's log...\n";
+    std::cout << "\n\n Upon boarding the abondoned alien vessel, you see a computer terminal" <<LevelDifficulty;
+    std::cout << "\n You need to enter the corret code to access the ship's log...\n";
 }
-void PlayGame(){
+bool PlayGame(){
     PrintIntroduction();
 
     const int CodeA = 4; 
@@ -39,16 +38,34 @@ void PlayGame(){
     if (CodeSum == GuessSum && CodeProduct == GuessProduct){
         std::cout << "Access Granted.\n";
         std::cout << "The terminal unlocks.\n";
+
+        return true;
     } else{
         std::cout << "Access Denied.\n";
         std::cout << "The terminal remains locked. The number '2' appears on the side of the terminal.\n" <<
         "You interpret this as having two remaining attempts to unlock the terminal until a security protocal is initiated...whatever that may mean.\n" <<
         "Would you like to continue?";
+
+        return false;
     }
 }
 
 int main(){
-    PlayGame();
-/*RETURN STATEMENT*/
+    int LevelDifficulty = 1;
+    while (true){
+        bool bLevelComplete = PlayGame(); //All boolean variables must start with lowercase 'b' in Unreal Engine
+
+        // notice if input letter for GuessCode creates endless loop --> this causes an error
+        std::cin.clear(); //Clears any errors
+        std::cin.ignore(); //Discards buffer
+
+        if (bLevelComplete){
+            //increase level difficulty
+            LevelDifficulty ++;
+
+        }
+        
+    }
+
     return 0; 
 }
