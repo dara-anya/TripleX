7b4271ef5e6588765e85ea874b822740cc2eb9a4
Dara Anya <daraanya@gmail.com>
2021-12-21 21:15:06 +0100

    commit - need to work on Attempts counter and Lose Function

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,5 +1,6 @@
 #include <iostream> 
 #include <ctime> // accesses computer's time
+#include <windows.h>
 
 /* 
 In this game there are three numbers in a code
@@ -9,27 +10,45 @@ The user must guess the three digit code
 */
 
 void PrintIntroduction(int Difficulty){
-    std::cout << "\n\nUpon boarding the abondoned alien vessel, you see a level " << Difficulty;
-    std::cout << " computer terminal. \n\nYou need to enter the corret code to access the ship's log...\n";
+    std::cout << "\n\nA distress signal has brought you aboard an abondoned alien vessel. You see a computer terminal. " <<
+    "There are three unilluminated lights above the terminal." << 
+    "\nYou need to enter the corret code to unlock the terminal, access the ship's log, and find out what happened...\n";
 }
-bool PlayGame(int Difficulty){
-    PrintIntroduction(Difficulty);
+
+int PlayAttempts (int Attempts){
+    int Attempts = 3;
+    Attempts--;
+    return Attempts;
+    std::cout << Attempts;
+   
+}
+
+bool PlayGame(int Difficulty, int Attempts){
 
     const int CodeA = rand() % Difficulty + Difficulty; //Add '1' to the modular to remove '0' from the randomization range
     const int CodeB = rand() % Difficulty + Difficulty;
     const int CodeC = rand() % Difficulty + Difficulty;
 
-    std::cout << CodeA << CodeB << CodeC;
+    //std::cout << CodeA << CodeB << CodeC;
 
     const int CodeSum = CodeA + CodeB + CodeC; 
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << "\nThere are three numbers in the code\n";
-    std::cout << "\nThe codes add-up to: " << CodeSum;
-    std::cout << "\nThe codes multiply to give: " << CodeProduct;
+    std::cout << "\nThe \"Level " << Difficulty;
+    std::cout << "\" terminal is locked by a three-digit code.";
+    std::cout << "\nA message on the side of the terminal reads:";
+    std::cout << "\n\n\tThe code adds-up to: " << CodeSum;
+    std::cout << "\n\tThe code multiplies to give: " << CodeProduct;
 
+    static bool initialized; // Executes once upon initialization
+    if (!initialized) {
+        initialized = true;
+        std::cout << "\n\nApparently, mathematics is a pillar of this species' civilization.";
+    }
+   
+    
     int GuessA, GuessB, GuessC; 
-    std::cout << "\n\nGuess the three-number code. Insert a space between each number:\n";
+    std::cout << "\n\n\tGuess the three-digit code. Insert a space between each digit:\n";
     // Store player guess
     std::cin >> GuessA >> GuessB >> GuessC;
 
@@ -39,15 +58,20 @@ bool PlayGame(int Difficulty){
 
     //Check if the player's guess is correct 
     if (CodeSum == GuessSum && CodeProduct == GuessProduct){
-        std::cout << "Access Granted.\n";
-        std::cout << "The terminal unlocks.\n";
+        Beep(1000,500);
+        std::cout << "\nYou hear a tone. One of the lights illuminate.";
+        std::cout << "\nYou have input the correct code."; 
 
         return true;
     } else{
-        std::cout << "Access Denied.\n";
-        std::cout << "The terminal remains locked. The number '2' appears on the side of the terminal.\n" <<
-        "You interpret this as having two remaining attempts to unlock the terminal until a security protocal is initiated...whatever that may mean.\n" <<
-        "Would you like to continue?";
+        Beep(100,500);
+        Beep(100,500);
+        std::cout << "\nYou hear a tone. No lights illuminate.";
+        std::cout << "\nYou have input the incorrect code.";
+
+        PlayAttempts(Attempts);
+        std::cout << "\n\nThe number " << Attempts << " appears on the terminal. You interpret this as having two more attempts to guess the correct code."; 
+        std::cout << "\nBe Careful. Who knows what will happen when all atempts are gone...\n";
 
         return false;
     }
@@ -57,10 +81,13 @@ int main(){
     srand(time(NULL)); //Creates new random sequences based on the time of day
     int LevelDifficulty = 1;
     const int MaxDifficulty = 3;
+    int Attempts = 3;
+
+    PrintIntroduction(LevelDifficulty);
 
     //Loop game until all levels completed
     while (LevelDifficulty <= MaxDifficulty){ 
-        bool bLevelComplete = PlayGame(LevelDifficulty); //All boolean variables must start with lowercase 'b' in Unreal Engine
+        bool bLevelComplete = PlayGame(LevelDifficulty, Attempts); //All boolean variables must start with lowercase 'b' in Unreal Engine
 
         // notice if input letter for GuessCode creates endless loop --> this causes an error
         std::cin.clear(); //Clears any errors
@@ -70,15 +97,17 @@ int main(){
             //increase level difficulty
             LevelDifficulty ++;
             if(((MaxDifficulty + 1)- LevelDifficulty) == 1){
-                std::cout << "There is " << ((MaxDifficulty + 1)- LevelDifficulty) << " lock remaining.";
+                std::cout << "\n" << ((MaxDifficulty + 1)- LevelDifficulty) << " lock remains.\n";
+                std::cout << "_________\n";
 
             } else{
-                std::cout << "There are " << ((MaxDifficulty + 1)- LevelDifficulty) << " locks remaining.";
+                std::cout << "\n" << ((MaxDifficulty + 1)- LevelDifficulty) << " locks remain.\n";
+                std::cout << "_________\n";
             }
 
         }
     }
 
-    std::cout << "You have access to the terminal";
+    std::cout << "\nYou have access to the terminal.";
     return 0; 
 }
